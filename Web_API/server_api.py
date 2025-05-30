a8a35ea0e9ad8f2c2d78f19d35a06ffc1ec633d8
Hhuynam <namha51023@gmail.com>
2025-05-30 07:04:41 +0700

    sửa serve dir

--- Web_API/server_api.py
+++ Web_API/server_api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)  # Cho phép CORS để frontend có thể thực hiện request
 
 # Đường dẫn tuyệt đối tới file JSON dự báo
-JSON_FORECAST_PATH = r"D:\HocKy3_2024-2025\KhoaHocDuDoanDanSo\Model\arima_population_forecast.json"
+JSON_FORECAST_PATH = r"D:\HocKy3_2024-2025\KhoaHocDuLieu\PhanTichDuDoanDanSo\Model\arima_population_forecast.json"
 # Đường dẫn file biểu đồ ACF/PACF
 CHART_PATH_1 = r"D:\HocKy3_2024-2025\KhoaHocDuLieu\PhanTichDuDoanDanSo\Model\acf_pacf_chart.png"
 # Đường dẫn file biểu đồ dự báo
